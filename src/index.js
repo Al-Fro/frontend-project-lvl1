127c4e76206beec998ec3b9f2bddefd615e11943
alfro <wwwalfro@mail.ru>
2020-03-11 20:59:33 +0300

    fixed eslint errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const hello = () => {
-    const greetings = readlineSync.question('May I have you Name?');
-    return "Hello, " + greetings; 
+export default () => {
+  const greetings = readlineSync.question('May I have you Name?');
+  return `Hello, ${greetings}`;
 };
-export default hello;
-
-
-
